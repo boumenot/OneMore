b5b200484d124f118f54e124929a23b7f4cfe02c
Christopher Boumenot <boumenot@gmail.com>
2023-09-02 09:15:34 -0700

    Extract test using Azure's Computer Vision service

--- OneMore/Commands/Images/ExtractTextCommand.cs
+++ OneMore/Commands/Images/ExtractTextCommand.cs
@@ -1,7 +1,13 @@
-﻿namespace River.OneMoreAddIn.Commands
+﻿//************************************************************************************************
+// Copyright © 2023 Christopher Boumenot.  All rights reserved.
+//************************************************************************************************
+
+using System.Collections.Concurrent;
+
+namespace River.OneMoreAddIn.Commands
 {
-    using River.OneMoreAddIn.Settings;
     using River.OneMoreAddIn.Models;
+    using River.OneMoreAddIn.UI;
 
     using System;
     using System.Collections.Generic;
@@ -20,9 +26,11 @@
         private Page page;
         private XNamespace ns;
 
+
         public ExtractTextCommand()
         {
         }
+
 
         public override async Task Execute(params object[] args)
         {
@@ -51,50 +59,98 @@
             {
                 UIHelper.ShowMessage(Resx.ResizeImagesDialog_noImages);
             }
-
-            await Task.FromResult(0);
         }
 
-        private async Task OcrImages(IEnumerable<XElement> elements)
+
+        private async Task OcrImages(XElement[] elements)
         {
-            foreach (var element in elements)
-            {
-                await OcrImage(element);
-            }
+            var dict = new ConcurrentDictionary<int, string>();
+
+            using var progress = new UI.ProgressDialog(2);
+            progress.Show();
+            progress.SetMessage("Extracting text from images...");
+
+            Parallel.ForEach(
+                elements.Select((x,i) => Tuple.Create(i, x)),
+                new ParallelOptions { MaxDegreeOfParallelism = 10 },
+                x =>
+                {
+                    var text = OcrImage(x.Item2).GetAwaiter().GetResult();
+                    dict.AddOrUpdate(x.Item1, text, (_, s) => s);
+                });
+
+            var text = string.Join("\n\n",
+                dict
+                    .OrderBy(x => x.Key)
+                    .Select(x => x.Value));
+
+            progress.Increment();
+            progress.SetMessage("Saving extracted text to the clipboard.");
+            this.SetClipboardText(text);
+            progress.Increment();
         }
 
-        private async Task OcrImage(XElement element)
+
+        private void SetClipboardText(string text)
+        {
+            var thread = new Thread(() => System.Windows.Clipboard.SetText(text));
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.IsBackground = false;
+            thread.Start();
+            thread.Join();
+        }
+
+
+        private async Task<string> OcrImage(XElement element)
         {
             using var image = ReadImage(element);
 
             string subscriptionKey = Environment.GetEnvironmentVariable("AZURE_COGNITIVE_SUBSCRIPTION_KEY");
-            string endpoint = "https://me.cognitiveservices.azure.com/";
+            string endpoint = Environment.GetEnvironmentVariable("AZURE_COGNITIVE_SUBSCRIPTION_ENDPOINT");
 
-            var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(subscriptionKey)) { Endpoint = endpoint };
-
-            var data = Convert.FromBase64String(element.Element(ns + "Data").Value);
-            using var stream = new MemoryStream(data, 0, data.Length);
-
-            var result = await client.ReadInStreamAsync(stream);
-
-            var operationId = Guid.Parse(result.OperationLocation.Substring(result.OperationLocation.Length - 36));
-
-            ReadOperationResult ror;
-            do
+            var client = new ComputerVisionClient(
+                new ApiKeyServiceClientCredentials(subscriptionKey),
+                HttpClientFactory.CreateNew(),
+                disposeHttpClient: false)
             {
-                ror = await client.GetReadResultAsync(operationId);
-                Thread.Sleep(TimeSpan.FromSeconds(3));
-            } while (ror.Status == OperationStatusCodes.Running || ror.Status == OperationStatusCodes.NotStarted);
+                Endpoint = endpoint
+            };
 
-            var text = string.Join("\n", ror.AnalyzeResult.ReadResults
-                .SelectMany(x => x.Lines)
-                .Select(x => x.Text)
-                .ToArray());
+            try
+            {
+                var data = Convert.FromBase64String(element.Element(ns + "Data")!.Value);
+                using var stream = new MemoryStream(data);
+
+                var result = await client.ReadInStreamAsync(stream).ConfigureAwait(false);
+
+                var operationId = Guid.Parse(result.OperationLocation.Substring(result.OperationLocation.Length - 36));
+
+                ReadOperationResult ror;
+                do
+                {
+                    ror = await client.GetReadResultAsync(operationId).ConfigureAwait(false);
+                    Thread.Sleep(TimeSpan.FromSeconds(1.5));
+                } while (ror.Status == OperationStatusCodes.Running || ror.Status == OperationStatusCodes.NotStarted);
+
+                var text = string.Join("\n", ror.AnalyzeResult.ReadResults
+                    .SelectMany(x => x.Lines)
+                    .Select(x => x.Text)
+                    .ToArray());
+
+                return text;
+            }
+            catch (Exception e)
+            {
+                this.logger.WriteVerbose(e.ToString());
+                return string.Empty;
+                // throw
+            }
         }
+
 
         private Stream ReadImage(XElement image)
         {
-            var data = Convert.FromBase64String(image.Element(ns + "Data").Value);
+            var data = Convert.FromBase64String(image.Element(ns + "Data")!.Value);
             return new MemoryStream(data, 0, data.Length);
         }
     }
